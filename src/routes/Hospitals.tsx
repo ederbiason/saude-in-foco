35f0466f7e0493ec39ac0b42a6123997aa1c3d67
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2024-05-01 16:13:02 -0300

    fix: fix current location on leaflet map

--- src/routes/Hospitals.tsx
+++ src/routes/Hospitals.tsx
@@ -1,9 +1,17 @@
 import { Map } from "@/components/Map";
+import { LatLngTuple } from "leaflet";
+import { useState } from "react";
 
 export default function Hospitals() {
+    const [coord, setCoord] = useState<LatLngTuple>([0,0])
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        setCoord([position.coords.latitude, position.coords.longitude])
+    })
+
     return (
         <div className="w-full h-full">
-            <Map center={[-23.3045, -51.1696]} />
+            <Map center={coord} />
         </div>
     )
 }
